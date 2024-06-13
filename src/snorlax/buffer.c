ab2198739bbf46125b060dbc73136732354f3135
snorlax <ceo@snorlax.bio>
2024-06-14 00:01:49 +0900

    Implement Buffer Method

--- src/snorlax/buffer.c
+++ src/snorlax/buffer.c
@@ -8,14 +8,35 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "buffer.h"
+
+#define page        8
 
 extern buffer_t * buffer_gen(void) {
     buffer_t * o = (buffer_t *) calloc(1, sizeof(buffer_t));
 
-    o->mem = malloc(4096);
-    o->capacity = 4096;
-
     return o;
+}
+
+extern buffer_t * buffer_rem(buffer_t * o) {
+    if(o->mem) free(o->mem);
+    free(o);
+    return nil;
+}
+
+extern void buffer_write(buffer_t * o, uint8_t * data, uint64_t n) {
+    buffer_reserve(o, n);
+
+    memcpy(&o->mem[o->size], data, n);
+}
+
+extern void buffer_reserve(buffer_t * o, uint64_t n) {
+    if(buffer_remain(o) < n) {
+        n = ((o->size + n) / page + 1) * page;
+
+        o->mem = (uint8_t *) (o->mem ? realloc(o->mem, n) : malloc(n));
+        o->capacity = n;
+    }
 }
--- src/snorlax/buffer.h
+++ src/snorlax/buffer.h
@@ -3,6 +3,9 @@
  * @brief
  * @details
  * 
+ * 필요할 때만 필요한 만큼 사용한다.
+ * 적은 바이트에 대해서 메모리 할당이 일어나지 않도록 한다.
+ * 
  * @author      snorlax <ceo@snorlax.bio>
  * @since       June 13, 2024
  */
@@ -24,5 +27,14 @@ struct buffer {
 };
 
 extern buffer_t * buffer_gen(void);
+extern buffer_t * buffer_rem(buffer_t * o);
+
+extern void buffer_write(buffer_t * o, uint8_t * data, uint64_t n);
+extern void buffer_reserve(buffer_t * o, uint64_t n);
+
+#define buffer_front(o)     (o->mem ? &o->mem[o->position] : nil)
+#define buffer_back(o)      (o->mem ? &o->mem[o->size] : nil)
+#define buffer_len(o)       (o->size - o->position)
+#define buffer_remain(o)    (o->capacity - o->size)
 
 #endif // __SNORLAX__BUFFER__H__
