37bd4568b26596a514f482bf01c1b5dbbe5783f1
snorlax <ceo@snorlax.bio>
2024-07-02 00:15:40 +0900

    Bug fix and Remove Print

--- src/snorlax/string/simple/deserializer.c
+++ src/snorlax/string/simple/deserializer.c
@@ -18,14 +18,12 @@ extern int64_t string_simple_deserialize(buffer_t * in, buffer_t * out) {
     snorlaxdbg(in == out, false, "critical", "");
 #endif // RELEASE
 
-    printf("1\n");
     if(buffer_remain(in) == 0){
         uint64_t capacity = buffer_size_get(in) + 1;
         capacity = (capacity / 8 + 1)* 8;
         buffer_capacity_set(in, capacity);
     }
 
-    printf("2\n");
     char * s = buffer_back(in);
 
     s[buffer_size_get(in)] = 0;
@@ -33,11 +31,9 @@ extern int64_t string_simple_deserialize(buffer_t * in, buffer_t * out) {
     s = buffer_front(in);
 
     if(s) {
-        printf("3\n");
         char * ret = index(s, '\n');
 
         if(ret != nil) {
-            printf("4\n");
             uint64_t n = ret - s;
 
             if(buffer_remain(out) < n) {
@@ -45,16 +41,13 @@ extern int64_t string_simple_deserialize(buffer_t * in, buffer_t * out) {
                 capacity = ((capacity + 1) / 8 + 1) * 8;
                 buffer_capacity_set(out, capacity);
             }
-            printf("5\n");
 
             memcpy(buffer_back(out), buffer_front(in), n);
             buffer_size_set(out, buffer_size_get(out) + n);
             buffer_position_set(in, buffer_position_get(in) + n);
-            printf("6\n");
 
             s = buffer_back(out);
             s[buffer_size_get(out)] = 0;
-            printf("7\n");
 
             return n;
         }
