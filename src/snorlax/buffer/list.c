3ca7a1045514456dbd0029ceec531096c5c82c78
snorlax <ceo@snorlax.bio>
2024-07-16 19:33:54 +0900

    Temporary Impl List Buffer

--- src/snorlax/buffer/list.c
+++ src/snorlax/buffer/list.c
@@ -1,7 +1,36 @@
 #include "list.h"
 
-static buffer_list_func_t func = {
+    // void (*adjust)(buffer_list_t *, uint64_t);
+    // void (*write)(buffer_list_t *, const uint8_t *, uint64_t);
 
+static buffer_list_t * buffer_list_func_rem(buffer_list_t * buffer);
+static uint8_t * buffer_list_func_front(buffer_list_t * buffer);
+static uint8_t * buffer_list_func_back(buffer_list_t * buffer);
+static uint64_t buffer_list_func_position_get(buffer_list_t * buffer);
+
+static void buffer_list_func_position_set(buffer_list_t * buffer, uint64_t v);
+static uint64_t buffer_list_func_size_get(buffer_list_t * buffer);
+static void buffer_list_func_size_set(buffer_list_t * buffer, uint64_t v);
+static uint64_t buffer_list_func_capacity_get(buffer_list_t * buffer);
+static void buffer_list_func_capacity_set(buffer_list_t * buffer, uint64_t v);
+static void buffer_list_func_reset(buffer_list_t * buffer, uint64_t n);
+static uint64_t buffer_list_func_remain(buffer_list_t * buffer);
+static uint64_t buffer_list_func_length(buffer_list_t * buffer);
+static void buffer_list_func_adjust(buffer_list_t * buffer, uint64_t n);
+static void buffer_list_func_write(buffer_list_t * buffer, const uint8_t * data, uint64_t n);
+
+static buffer_list_func_t func = {
+    buffer_list_func_rem,
+    buffer_list_func_front,
+    buffer_list_func_back,
+    buffer_list_func_position_get,
+    buffer_list_func_position_set,
+    buffer_list_func_size_get,
+    buffer_list_func_reset,
+    buffer_list_func_remain,
+    buffer_list_func_length,
+    buffer_list_func_adjust,
+    buffer_list_func_write
 };
 
 extern buffer_list_t * buffer_list_gen(void) {
@@ -10,4 +39,187 @@ extern buffer_list_t * buffer_list_gen(void) {
     buffer->func = address_of(func);
 
     return buffer;
+}
+
+static buffer_list_t * buffer_list_func_rem(buffer_list_t * buffer) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    buffer_list_node_t * node = buffer->head;
+    while(buffer->head) {
+        buffer->head = node->next;
+
+        node->prev = nil;
+        node->next = nil;
+        node->collection = nil;
+
+        buffer_list_node_rem(node);
+
+        node = buffer->head;
+    }
+
+    buffer->sync = sync_rem(buffer->sync);
+    
+    free(buffer);
+
+    return nil;
+}
+
+static uint8_t * buffer_list_func_front(buffer_list_t * buffer) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    // TODO: UPGRADE
+    return buffer_list_node_mem(buffer->front);
+}
+
+static uint8_t * buffer_list_func_back(buffer_list_t * buffer) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    // TODO: UPGRADE
+    return buffer_list_node_mem(buffer->back);
+}
+
+static uint64_t buffer_list_func_position_get(buffer_list_t * buffer) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    return (uint64_t) buffer->front;
+}
+
+static void buffer_list_func_position_set(buffer_list_t * buffer, uint64_t v) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    buffer->front = (buffer_list_node_t *) v;
+}
+
+static uint64_t buffer_list_func_size_get(buffer_list_t * buffer) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    return (uint64_t) buffer->back;
+}
+
+static void buffer_list_func_size_set(buffer_list_t * buffer, uint64_t v) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    buffer->back = (buffer_list_node_t *) v;
+}
+
+static uint64_t buffer_list_func_capacity_get(buffer_list_t * buffer) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    return (uint64_t) buffer->tail;
+}
+
+static void buffer_list_func_capacity_set(buffer_list_t * buffer, uint64_t v) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    buffer->tail = (buffer_list_node_t *) v;
+}
+
+static void buffer_list_func_reset(buffer_list_t * buffer, uint64_t n) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+    snorlaxdbg(n > 0, false, "critical", "");
+#endif // RELEASE
+
+    buffer_list_node_t * node = buffer->head;
+
+    while(buffer->head) {
+        buffer->head = node->next;
+
+        node->prev = nil;
+        node->next = nil;
+        node->collection = nil;
+
+        buffer_list_node_rem(node);
+
+        node = buffer->head;
+    }
+
+    buffer->size = 0;
+    buffer->head = nil;
+    buffer->tail = nil;
+    buffer->front = nil;
+    buffer->back = nil;
+}
+
+static uint64_t buffer_list_func_remain(buffer_list_t * buffer) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    return buffer->tail != buffer->back && buffer->back != nil;
+}
+
+static uint64_t buffer_list_func_length(buffer_list_t * buffer) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    return buffer->front != nil && buffer->front != buffer->back;
+}
+
+static void buffer_list_func_adjust(buffer_list_t * buffer, uint64_t n) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+    snorlaxdbg(n > 0, false, "critical", "");
+#endif // RELEASE
+
+    while(buffer->head && buffer_list_node_done(buffer->head)) {
+        buffer_list_node_t * node = buffer->head;
+
+#ifndef   RELEASE
+        snorlaxdbg(buffer->front == node, false, "critical", "");
+        snorlaxdbg(buffer->back == node, false, "critical", "");
+#endif // RELEASE
+
+        buffer->head = node->next;
+        if(buffer->head) {
+            buffer->head->prev = nil;
+        } else {
+            buffer->tail = nil;
+        }
+        buffer->size = buffer->size - 1;
+        node->next = nil;
+        node->collection = nil;
+
+        buffer_list_node_rem(node);
+    }
+}
+
+static void buffer_list_func_write(buffer_list_t * buffer, const uint8_t * data, uint64_t n) {
+#ifndef   RELEASE
+    snorlaxdbg(buffer == nil, false, "critical", "");
+#endif // RELEASE
+
+    buffer_list_node_t * node = buffer_list_node_gen(data, n);
+
+    if(buffer->tail) {
+        buffer->tail->next = node;
+        node->prev = buffer->tail;
+    } else {
+        buffer->head = node;
+    }
+
+    buffer->tail = node;
+    buffer->size = buffer->size + 1;
+    node->collection = buffer;
+
+    buffer->back = nil;
 }
--- src/snorlax/buffer/list.h
+++ src/snorlax/buffer/list.h
@@ -15,12 +15,25 @@
 struct buffer_list;
 struct buffer_list_func;
 
+struct buffer_list_node;
+struct buffer_list_node_func;
+
 typedef struct buffer_list buffer_list_t;
 typedef struct buffer_list_func buffer_list_func_t;
+
+typedef struct buffer_list_node buffer_list_node_t;
+typedef struct buffer_list_node_func buffer_list_node_func_t;
 
 struct buffer_list {
     buffer_list_func_t * func;
     sync_t * sync;
+
+    uint64_t size;
+    buffer_list_node_t * head;
+    buffer_list_node_t * tail;
+
+    buffer_list_node_t * front;
+    buffer_list_node_t * back;
 };
 
 struct buffer_list_func {
@@ -40,6 +53,21 @@ struct buffer_list_func {
     void (*write)(buffer_list_t *, const uint8_t *, uint64_t);
 };
 
+struct buffer_list_node {
+    buffer_list_node_func_t * func;
+    sync_t * sync;
+    buffer_list_t * collection;
+    buffer_list_node_t * prev;
+    buffer_list_node_t * next;
+};
+
+struct buffer_list_node_func {
+    buffer_list_node_t * (*rem)(buffer_list_node_t *);
+
+    uint8_t * (*mem)(buffer_list_node_t *);
+    int32_t (*done)(buffer_list_node_t *);
+};
+
 extern buffer_list_t * buffer_list_gen(void);
 
 #define buffer_list_rem(buffer)                 ((buffer)->func->rem(buffer))
@@ -56,5 +84,9 @@ extern buffer_list_t * buffer_list_gen(void);
 #define buffer_list_length(buffer)              ((buffer)->func->length(buffer))
 #define buffer_list_adjust(buffer, n)           ((buffer)->func->adjust(buffer, n))
 #define buffer_list_write(buffer, data, len)    ((buffer)->func->write(buffer, data, len))
+
+#define buffer_list_node_rem(node)              ((node)->func->rem(node))
+#define buffer_list_node_mem(node)              ((node)->func->mem(node))
+#define buffer_list_node_done(node)             ((node)->func->done(node))
 
 #endif // __SNORLAX__BUFFER_LIST__H__
